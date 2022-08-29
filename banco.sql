72ca52fa4a67e43bfcb5f31305633c1abbb700a5
michel-costa <michel.mcs.admpf@gmail.com>
2022-08-28 21:40:30 -0300

    Atualização de Banco de Dados

---
+++ banco.sql
@@ -0,0 +1,24 @@
+CREATE TABLE dados (
+    AlunoID int,
+    Nome varchar(50),
+    Sobrenome varchar(50),
+    Endereco varchar(150),
+    Cidade varchar(50),
+    Host varchar(50)
+);
+
+create table clientes (
+maria varchar(30),
+lucio varchar(30),
+helena varchar(30),
+eduardo varchar(30),
+mario varchar(30)
+);
+
+create table departamento (
+recursos humanos varchar(30),
+financas varchar(30),
+patrimonio varchar(30),
+administracao varchar(30),
+tecnologia da informacao varchar(30)
+);
